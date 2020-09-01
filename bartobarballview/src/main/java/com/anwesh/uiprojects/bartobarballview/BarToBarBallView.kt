8d5eadd803798148c6d5b1dc0ad343c26555bc67
Anwesh43 <anweshthecool0@gmail.com>
2020-09-02 02:31:55 +0530

    adding constants and libraries

--- bartobarballview/src/main/java/com/anwesh/uiprojects/bartobarballview/BarToBarBallView.kt
+++ bartobarballview/src/main/java/com/anwesh/uiprojects/bartobarballview/BarToBarBallView.kt
@@ -3,3 +3,26 @@ package com.anwesh.uiprojects.bartobarballview
 /**
  * Created by anweshmishra on 02/09/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#2196F3",
+        "#4CAF50",
+        "#3F51B5",
+        "#673AB7"
+).map({Color.parseColor(it)}).toTypedArray()
+val circles : Int = 3
+val scGap : Float = 0.02f / (circles * 2)
+val delay : Long = 20
+val sizeFactor : Float = 2.9f
+val hFactor : Float = 5.2f
+val backColor : Int = Color.parseColor("#BDBDBD")
