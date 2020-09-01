9ecee6e3e7888d22e31da4029b3b9a40c3f8ee0b
Anwesh43 <anweshthecool0@gmail.com>
2020-09-02 03:04:21 +0530

    adding BTBBNode class

--- bartobarballview/src/main/java/com/anwesh/uiprojects/bartobarballview/BarToBarBallView.kt
+++ bartobarballview/src/main/java/com/anwesh/uiprojects/bartobarballview/BarToBarBallView.kt
@@ -131,4 +131,45 @@ class BarToBarBallView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BTBBNode(var i : Int, val state : State = State()) {
+
+        private var next : BTBBNode? = null
+        private var prev : BTBBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BTBBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBTBBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BTBBNode {
+            var curr : BTBBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
